061370cc88b2e00e6eaffdd30bf30f5dcaa6bb03
Theodore L'Heureux <theodorelheureux@gmail.com>
2022-10-31 09:52:59 -0400

    feat: read file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let path = &args[2];
+
+    let content = fs::read_to_string(path).expect("Error while reading file.");
+
+    println!("Contents : \n{}", content);
 }

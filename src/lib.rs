102c47964b59414dab397e28b05c881073d9b014
Theodore L'Heureux <theodorelheureux@gmail.com>
2022-11-01 15:26:25 -0400

    feat: add support for piped text

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,119 @@
+use atty::Stream;
 use std::error::Error;
-use std::fs;
+use std::io::{stdin, BufRead, Stdin};
+use std::{fs, io};
 
-pub struct Config<'a> {
-    pub query: &'a String,
-    pub path: &'a String,
+pub struct Config {
+    pub query: String,
+    pub path: String,
 }
 
-impl<'a> Config<'a> {
-    pub fn build(args: &'a [String]) -> Result<Config, &'static str> {
+impl Config {
+    pub fn build(args: Box<[String]>) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Not enough arguments.");
         }
 
-        let query = &args[1];
-        let path = &args[2];
-
-        Ok(Config { query, path })
+        Ok(Config {
+            query: args[1].clone(),
+            path: args[2].clone(),
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.path)?;
 
-    println!("Contents of {}: \n{}", config.query, content);
+    for line in search(&config.query, &content) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn get_piped() -> io::Result<String> {
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock(); // locking is optional
+
+    let mut line = String::new();
+
+    // Could also `match` on the `Result` if you wanted to handle `Err`
+    while let Ok(n_bytes) = stdin.read_line(&mut line) {
+        if n_bytes == 0 {
+            break;
+        }
+        println!("{}", line);
+        line.clear();
+    }
+    Ok(line)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_build() {
+        let args = vec![
+            String::from("target/debug/rust_grep"),
+            String::from("query"),
+            String::from("path"),
+        ];
+
+        let config = Config::build(Box::from(args.clone())).unwrap();
+
+        assert_eq!(config.query, args[1]);
+        assert_eq!(config.path, args[2]);
+
+        let args = vec![
+            String::from("target/debug/rust_grep"),
+            String::from("43244dgdsd"),
+            String::from("243rewfdd"),
+        ];
+
+        let config = Config::build(Box::from(args.clone())).unwrap();
+
+        assert_eq!(config.query, args[1]);
+        assert_eq!(config.path, args[2]);
+    }
+
+    #[test]
+    fn config_build_not_enough_args() {
+        let args = vec![String::from("target/debug/rust_grep")];
+
+        let config = Config::build(Box::from(args));
+
+        assert!(config.is_err());
+
+        let args = vec![
+            String::from("target/debug/rust_grep"),
+            String::from("query"),
+        ];
+
+        let config = Config::build(Box::from(args));
+
+        assert!(config.is_err());
+    }
+
+    #[test]
+    fn one_result() {
+        let query = String::from("duct");
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(&query, &contents));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use rust_grep::Config;
 use std::env;
+use std::io;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    println!("{}", rust_grep::get_piped().unwrap());
 
-    println!("Searching for {} in {}:\n", config.query, config.path);
+    // let args: Vec<String> = env::args().collect();
+    // let stdin = rust_grep::get_stdin(io::stdin());
+    // let mut config: Option<Config> = None;
 
-    if let Err(e) = rust_grep::run(config) {
-        println!("Application error: {e}");
-        process::exit(1);
-    }
+    // if let Ok(content) = stdin {
+    //     config = Some(
+    //         Config::build(Box::from([String::new(), content, args[1].clone()])).unwrap_or_else(
+    //             |err| {
+    //                 println!("Problem parsing arguments: {err}");
+    //                 process::exit(1);
+    //             },
+    //         ),
+    //     );
+    // }
+    // if let None = config {
+    //     config = Some(Config::build(Box::from(args)).unwrap_or_else(|err| {
+    //         println!("Problem parsing arguments: {err}");
+    //         process::exit(1);
+    //     }));
+    // }
+
+    // let config = config.unwrap();
+
+    // if let Err(e) = rust_grep::run(config) {
+    //     println!("Application error: {e}");
+    //     process::exit(1);
+    // }
 }

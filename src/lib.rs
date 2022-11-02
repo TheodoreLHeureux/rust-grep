883cf357a0b9086df2f77c3fc176113c7afd14f1
Théodore L'Heureux <theodorelheureux@gmail.com>
2022-11-02 12:15:00 -0400

    feat: add support for help parameter

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::{env, process};
 use std::error::Error;
 use std::fs;
 use std::io::{self, BufRead};
@@ -5,11 +6,21 @@ use std::io::{self, BufRead};
 pub struct Config {
     pub query: String,
     pub path: String,
-    content: Option<String>,
+    pub content: Option<String>,
+    pub ignore_case: bool,
 }
 
 impl Config {
-    pub fn build(args: Vec<String>, content: Option<String>) -> Result<Config, &'static str> {
+    pub fn build(mut args: Vec<String>, content: Option<String>) -> Result<Config, &'static str> {
+        let params = extract_params(&mut args);
+
+        if params.contains(&"--help".to_string()) || params.contains(&"-h".to_string()) {
+
+            println!("Usage: rust-grep [OPTION] [QUERY] [FILE]");
+        
+            process::exit(1);
+        }
+
         if (args.len() < 3 && content == None) || args.len() < 2 {
             return Err("Not enough arguments.");
         }
@@ -20,24 +31,56 @@ impl Config {
             path = args[2].clone();
         }
 
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         Ok(Config {
             query: args[1].clone(),
             path,
             content,
+            ignore_case,
         })
     }
 }
 
-pub fn run(mut config: Config) -> Result<(), Box<dyn Error>> {
-    if let None = config.content {
-        config.content = Some(fs::read_to_string(config.path)?);
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content: String;
+
+    match config.content {
+        None => content = fs::read_to_string(config.path)?,
+        Some(v) => content = v,
     }
 
-    for line in search(&config.query, &config.content.unwrap()) {
+    let result: Vec<&str>;
+
+    if config.ignore_case {
+        result = search_case_insensitive(&config.query, &content)
+    } else {
+        result = search(&config.query, &content);
+    }
+
+    for line in result {
         println!("{line}");
     }
 
     Ok(())
+}
+
+pub fn extract_params(args: &mut Vec<String>) -> Vec<String> {
+    let mut params = Vec::new();
+    let mut indices: Vec<usize> = Vec::new();
+
+    for i in 0..args.len() {
+        if args[i].starts_with('-') {
+            indices.push(i);
+            params.push(args[i].clone());
+        }
+    }
+
+    for i in indices {
+        args.remove(i);
+    }
+
+    params
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
@@ -45,6 +88,18 @@ pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
 
     for line in content.lines() {
         if line.contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
             results.push(line);
         }
     }
@@ -126,5 +181,32 @@ Rust:
 safe, fast, productive.
 Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(&query, &contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
